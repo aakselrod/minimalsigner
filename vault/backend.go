59f04b784d20fb657efbd386c22af8733675e9ea
Alex Akselrod <alex@akselrod.org>
2022-10-06 16:02:57 -0700

    wip vault plugin

--- vault/backend.go
+++ vault/backend.go
@@ -1,11 +1,16 @@
 package vault
 
 import (
+	"bytes"
 	"context"
 	"encoding/hex"
 	"errors"
+	"fmt"
 
 	"github.com/aakselrod/minimalsigner/keyring"
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcec/v2/ecdsa"
+	"github.com/btcsuite/btcd/btcec/v2/schnorr"
 	"github.com/btcsuite/btcd/btcutil/hdkeychain"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/hashicorp/vault/sdk/framework"
@@ -23,6 +28,7 @@ func (b *backend) paths() []*framework.Path {
 			Callbacks: map[logical.Operation]framework.OperationFunc{
 				logical.ListOperation:   b.listNodes,
 				logical.UpdateOperation: b.createNode,
+				logical.CreateOperation: b.createNode,
 			},
 			HelpSynopsis: "Create and list LND nodes",
 			HelpDescription: `
@@ -42,7 +48,231 @@ POST - generate a new node seed and store it indexed by node pubkey
 				},
 			},
 		},
+		&framework.Path{
+			Pattern: "lnd-nodes/sign/?",
+			Callbacks: map[logical.Operation]framework.OperationFunc{
+				logical.ReadOperation:   b.derivePubKey,
+				logical.UpdateOperation: b.deriveAndSign,
+				logical.CreateOperation: b.deriveAndSign,
+			},
+			HelpSynopsis: "Derive pubkeys and sign with privkeys",
+			HelpDescription: `
+
+GET  - return the pubkey derived with the submitted path
+POST - sign a digest with the method specified using the privkey derived with
+the submitted path
+
+`,
+			Fields: map[string]*framework.FieldSchema{
+				"node": &framework.FieldSchema{
+					Type: framework.TypeString,
+					Description: "node pubkey, must be " +
+						"66 hex characters",
+					Default: "",
+				},
+				"path": &framework.FieldSchema{
+					Type: framework.TypeCommaIntSlice,
+					Description: "derivation path, with " +
+						"the first 3 elements " +
+						"being hardened",
+					Default: []int{},
+				},
+				"digest": &framework.FieldSchema{
+					Type: framework.TypeString,
+					Description: "digest to sign, must " +
+						"be hex-encoded 32 bytes",
+					Default: "",
+				},
+				"method": &framework.FieldSchema{
+					Type: framework.TypeString,
+					Description: "signing method: " +
+						"one of: ecdsa, " +
+						"ecdsa-compact, or schnorr",
+					Default: "",
+				},
+				"pubkey": &framework.FieldSchema{
+					Type: framework.TypeString,
+					Description: "optional: pubkey for " +
+						"which to sign, checked " +
+						"against derived pubkey to " +
+						"ensure a match",
+					Default: "",
+				},
+			},
+		},
 	}
+}
+
+func (b *backend) derivePubKey(ctx context.Context, req *logical.Request,
+	data *framework.FieldData) (*logical.Response, error) {
+
+	strNode := data.Get("node").(string)
+
+	seed, net, err := b.getNode(ctx, req.Storage, strNode)
+	if err != nil {
+		b.Logger().Error("Failed to retrieve node info",
+			"node", strNode, "error", err)
+		return nil, err
+	}
+	defer zero(seed)
+
+	derivationPath := data.Get("path").([]uint32)
+	pubKey, err := derivePubKey(seed, net, derivationPath)
+	if err != nil {
+		b.Logger().Error("Failed to derive pubkey",
+			"node", strNode, "derivation_path", derivationPath,
+			"error", err)
+		return nil, err
+	}
+
+	pubKeyBytes, err := extKeyToPubBytes(pubKey)
+	if err != nil {
+		b.Logger().Error("Failed to serialize pubkey to hex",
+			"node", strNode, "derivation_path", derivationPath,
+			"error", err)
+		return nil, err
+	}
+
+	return &logical.Response{
+		Data: map[string]interface{}{
+			"nodePubKey": hex.EncodeToString(pubKeyBytes),
+		},
+	}, nil
+}
+
+func (b *backend) deriveAndSign(ctx context.Context, req *logical.Request,
+	data *framework.FieldData) (*logical.Response, error) {
+
+	strNode := data.Get("node").(string)
+
+	seed, net, err := b.getNode(ctx, req.Storage, strNode)
+	if err != nil {
+		b.Logger().Error("Failed to retrieve node info",
+			"node", strNode, "error", err)
+		return nil, err
+	}
+	defer zero(seed)
+
+	derivationPath := data.Get("path").([]uint32)
+	privKey, err := derivePrivKey(seed, net, derivationPath)
+	if err != nil {
+		b.Logger().Error("Failed to derive privkey",
+			"node", strNode, "derivation_path", derivationPath,
+			"error", err)
+		return nil, err
+	}
+	defer privKey.Zero()
+
+	ecPrivKey, err := privKey.ECPrivKey()
+	if err != nil {
+		b.Logger().Error("Failed to derive valid ECDSA privkey",
+			"node", strNode, "derivation_path", derivationPath,
+			"error", err)
+		return nil, err
+	}
+	defer ecPrivKey.Zero()
+
+	pubKeyBytes, err := extKeyToPubBytes(privKey)
+	if err != nil {
+		b.Logger().Error("Failed to get pubkey bytes",
+			"node", strNode, "derivation_path", derivationPath,
+			"error", err)
+		return nil, err
+	}
+
+	reqPubKey := data.Get("pubkey").(string)
+
+	if reqPubKey != "" {
+		reqPubBytes, err := hex.DecodeString(reqPubKey)
+		if err != nil {
+			b.Logger().Error("Failed to decode requested pubkey ",
+				"hex", "error", err)
+			return nil, err
+		}
+
+		// This is expected when we're checking if a PSBT input is ours
+		// so the severity is just info.
+		if !bytes.Equal(reqPubBytes, pubKeyBytes) {
+			b.Logger().Info("Requested pubkey didn't match "+
+				"derived pubkey", "requested", reqPubKey)
+			return nil, errors.New("pubkey mismatch")
+		}
+	}
+
+	digest := data.Get("digest").(string)
+	digestBytes, err := hex.DecodeString(digest)
+	if err != nil {
+		b.Logger().Error("Failed to decode digest from hex",
+			"error", err)
+		return nil, err
+	}
+
+	var sigBytes []byte
+
+	// TODO(aakselrod): check derivation paths are sane for the type of
+	// signature we're requesting.
+	signMethod := data.Get("method").(string)
+	switch signMethod {
+	case "ecdsa":
+		sigBytes = ecdsa.Sign(ecPrivKey, digestBytes).Serialize()
+	case "ecdsa-compact":
+		sigBytes, _ = ecdsa.SignCompact(ecPrivKey, digestBytes, true)
+	case "schnorr":
+		sig, err := schnorr.Sign(ecPrivKey, digestBytes)
+		if err != nil {
+			b.Logger().Error("Failed to sign digest using Schnorr",
+				"node", strNode,
+				"derivation_path", derivationPath,
+				"pubkey", reqPubKey, "error", err)
+			return nil, err
+		}
+
+		sigBytes = sig.Serialize()
+	default:
+		b.Logger().Info("Requested invalid signing method",
+			"method", signMethod)
+		return nil, errors.New("invalid signing method")
+	}
+
+	return &logical.Response{
+		Data: map[string]interface{}{
+			"signature": hex.EncodeToString(sigBytes),
+		},
+	}, nil
+}
+
+func extKeyToPubBytes(key *hdkeychain.ExtendedKey) ([]byte, error) {
+	ecPubKey, err := key.ECPubKey()
+	if err != nil {
+		return nil, err
+	}
+
+	return ecPubKey.SerializeCompressed(), nil
+}
+
+func (b *backend) getNode(ctx context.Context, storage logical.Storage,
+	id string) ([]byte, *chaincfg.Params, error) {
+
+	if len(id) != 2*btcec.PubKeyBytesLenCompressed {
+		return nil, nil, errors.New("invalid node id")
+	}
+
+	nodePath := "lnd-nodes/" + id
+	entry, err := storage.Get(ctx, nodePath)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if len(entry.Value) <= hdkeychain.RecommendedSeedLen {
+		return nil, nil, errors.New("got invalid seed from storage")
+	}
+
+	net, err := getNet(string(entry.Value[hdkeychain.RecommendedSeedLen:]))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return entry.Value[:hdkeychain.RecommendedSeedLen], net, nil
 }
 
 func (b *backend) listNodes(ctx context.Context, req *logical.Request,
@@ -69,6 +299,8 @@ func (b *backend) createNode(ctx context.Context, req *logical.Request,
 	}
 
 	var seed []byte
+	defer zero(seed)
+
 	err = hdkeychain.ErrUnusableSeed
 	for err == hdkeychain.ErrUnusableSeed {
 		seed, err = hdkeychain.GenerateSeed(hdkeychain.RecommendedSeedLen)
@@ -79,32 +311,27 @@ func (b *backend) createNode(ctx context.Context, req *logical.Request,
 		return nil, err
 	}
 
-	nodePrivKey, err := derivePrivKey(seed, net, [5]uint32{
-		keyring.Bip0043purpose,
-		net.HDCoinType,
-		keyring.NodeKeyAcct,
+	nodePubKey, err := derivePubKey(seed, net, []uint32{
+		keyring.Bip0043purpose + hdkeychain.HardenedKeyStart,
+		net.HDCoinType + hdkeychain.HardenedKeyStart,
+		keyring.NodeKeyAcct + hdkeychain.HardenedKeyStart,
 		0,
 		0,
 	})
 	if err != nil {
-		b.Logger().Error("Failed to derive node privkey from LND seed",
+		b.Logger().Error("Failed to derive node pubkey from LND seed",
 			"error", err)
 		return nil, err
 	}
-	defer nodePrivKey.Zero()
 
-	pubKey, err := nodePrivKey.ECPubKey()
+	pubKeyBytes, err := extKeyToPubBytes(nodePubKey)
 	if err != nil {
-		b.Logger().Error("Failed to derive node pubkey from privkey",
+		b.Logger().Error("Failed to serialize pubkey to hex",
 			"error", err)
 		return nil, err
 	}
 
-	strPubKey := hex.EncodeToString(pubKey.SerializeCompressed())
-	if len(strPubKey) != 66 {
-		b.Logger().Error("Failed to get hex of node pubkey")
-		return nil, errors.New("Failed to get hex of node pubkey")
-	}
+	strPubKey := hex.EncodeToString(pubKeyBytes)
 	nodePath := "lnd-nodes/" + strPubKey
 
 	seed = append(seed, []byte(strNet)...)
@@ -150,8 +377,29 @@ func getNet(strNet string) (*chaincfg.Params, error) {
 	}
 }
 
+// assertHardened makes sure each given element is >= 2^31.
+func assertHardened(elements ...uint32) error {
+	for idx, element := range elements {
+		if element < hdkeychain.HardenedKeyStart {
+			return fmt.Errorf("element at index %d is not hardened",
+				idx)
+		}
+	}
+
+	return nil
+}
+
 func derivePrivKey(seed []byte, net *chaincfg.Params,
-	derivationPath [5]uint32) (*hdkeychain.ExtendedKey, error) {
+	derivationPath []uint32) (*hdkeychain.ExtendedKey, error) {
+
+	if len(derivationPath) != 5 {
+		return nil, errors.New("derivation path not 5 elements")
+	}
+
+	err := assertHardened(derivationPath[:2]...)
+	if err != nil {
+		return nil, err
+	}
 
 	rootKey, err := hdkeychain.NewMaster(seed, net)
 	if err != nil {
@@ -160,11 +408,8 @@ func derivePrivKey(seed []byte, net *chaincfg.Params,
 	defer rootKey.Zero()
 
 	// Derive purpose.
-	if derivationPath[0] > hdkeychain.HardenedKeyStart {
-		return nil, errors.New("purpose already hardened")
-	}
 	purposeKey, err := rootKey.DeriveNonStandard(
-		derivationPath[0] + hdkeychain.HardenedKeyStart,
+		derivationPath[0],
 	)
 	if err != nil {
 		return nil, errors.New("error deriving purpose")
@@ -172,11 +417,8 @@ func derivePrivKey(seed []byte, net *chaincfg.Params,
 	defer purposeKey.Zero()
 
 	// Derive coin type.
-	if derivationPath[1] > hdkeychain.HardenedKeyStart {
-		return nil, errors.New("coin type already hardened")
-	}
 	coinTypeKey, err := purposeKey.DeriveNonStandard(
-		derivationPath[1] + hdkeychain.HardenedKeyStart,
+		derivationPath[1],
 	)
 	if err != nil {
 		return nil, errors.New("error deriving coin type")
@@ -184,11 +426,8 @@ func derivePrivKey(seed []byte, net *chaincfg.Params,
 	defer coinTypeKey.Zero()
 
 	// Derive account.
-	if derivationPath[2] > hdkeychain.HardenedKeyStart {
-		return nil, errors.New("account already hardened")
-	}
 	accountKey, err := coinTypeKey.DeriveNonStandard(
-		derivationPath[2] + hdkeychain.HardenedKeyStart,
+		derivationPath[2],
 	)
 	if err != nil {
 		return nil, errors.New("error deriving account")
@@ -196,9 +435,6 @@ func derivePrivKey(seed []byte, net *chaincfg.Params,
 	defer accountKey.Zero()
 
 	// Derive branch.
-	if derivationPath[3] > hdkeychain.HardenedKeyStart {
-		return nil, errors.New("branch is hardened")
-	}
 	branchKey, err := accountKey.DeriveNonStandard(derivationPath[3])
 	if err != nil {
 		return nil, errors.New("error deriving branch")
@@ -206,15 +442,24 @@ func derivePrivKey(seed []byte, net *chaincfg.Params,
 	defer branchKey.Zero()
 
 	// Derive index.
-	if derivationPath[2] > hdkeychain.HardenedKeyStart {
-		return nil, errors.New("index is hardened")
-	}
 	indexKey, err := accountKey.DeriveNonStandard(derivationPath[4])
 	if err != nil {
 		return nil, errors.New("error deriving index")
 	}
 
 	return indexKey, nil
+}
+
+func derivePubKey(seed []byte, net *chaincfg.Params,
+	derivationPath []uint32) (*hdkeychain.ExtendedKey, error) {
+
+	privKey, err := derivePrivKey(seed, net, derivationPath)
+	if err != nil {
+		return nil, err
+	}
+	defer privKey.Zero()
+
+	return privKey.Neuter()
 }
 
 func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend,
@@ -239,4 +484,13 @@ func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend,
 	}
 
 	return &b, nil
+}
+
+// zero sets all bytes in the passed slice to zero.  This is used to
+// explicitly clear private key material from memory.
+func zero(b []byte) {
+	lenb := len(b)
+	for i := 0; i < lenb; i++ {
+		b[i] = 0
+	}
 }
